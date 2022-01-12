a64e6fcea986c77f9a54ff44f95c6550eda294fe
Evgeny-Mufteev <evgenymufteev@gmail.com>
2022-01-12 17:55:35 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+let dateEnd = new Date("2022-02-02 00:00:00"),
+    dateNow = new Date(),
+    date = Math.floor((dateEnd.getTime() - dateNow.getTime()) / 1000);
+
+function countdown() {
+    let dateLeft = date,
+        dateTemp = 0;
+
+    dateTemp = Math.floor(dateLeft / (24 * 60 * 60));
+    dateLeft -= dateTemp * 24 * 60 * 60;
+
+    if (dateTemp < 10) dateTemp = "0" + dateTemp;
+    document.querySelector("#days span").innerHTML = dateTemp;
+
+    dateTemp = Math.floor(dateLeft / (60 * 60));
+    dateLeft -= dateTemp * 60 * 60;
+    if (dateTemp < 10) dateTemp = "0" + dateTemp;
+    document.querySelector("#hours span").innerHTML = dateTemp;
+
+    dateTemp = Math.floor(dateLeft / 60);
+    dateLeft -= dateTemp * 60;
+    if (dateTemp < 10) dateTemp = "0" + dateTemp;
+    document.querySelector("#minutes span").innerHTML = dateTemp;
+
+    if (dateTemp < 10) dateTemp = "0" + dateTemp;
+    document.querySelector("#seconds span").innerHTML = dateLeft;
+
+    date--;
+}
+
+setInterval(countdown, 1000);
